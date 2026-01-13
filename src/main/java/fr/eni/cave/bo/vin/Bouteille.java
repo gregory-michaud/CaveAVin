9f7f788b9314452224b03caa19857ad74300aa08
Gregory <gmichaud@campus-eni.fr>
2026-01-13 11:12:27 +0100

    M04_TP05

--- src/main/java/fr/eni/cave/bo/vin/Bouteille.java
+++ src/main/java/fr/eni/cave/bo/vin/Bouteille.java
@@ -24,15 +24,15 @@ public class Bouteille {
     @Column(name = "BOTTLE_ID")
     private Integer id;
 
-    @NotBlank
-    @Size(max = 250)
+    @NotBlank(message = "{bottle.name.blank-error}")
+    @Size(max = 250, message = "{bottle.name.size-error}")
     @Column(name = "NAME", unique = true, nullable = false, length = 250)
     private String nom;
 
     @Column(name = "SPARKLING")
     private boolean petillant;
 
-    @Size(max = 100)
+    @Size(max = 100, message = "{bottle.vintage.size-error}")
     @Column(name = "VINTAGE", length = 100)
     private String millesime;
 
@@ -40,17 +40,17 @@ public class Bouteille {
     @Column(name = "QUANTITY")
     private int quantite;
 
-    @Min(value = 1)
+    @Min(value = 1, message = "{bottle.price.min-error}")
     @Column(name = "PRICE", precision = 2)
     private float prix;
 
-    @NotNull
+    @NotNull(message = "{bottle.color.not-null}")
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "COLOR_ID")
     @EqualsAndHashCode.Exclude
     private Couleur couleur;
 
-    @NotNull
+    @NotNull(message = "{bottle.region.not-null}")
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "REGION_ID")
     @EqualsAndHashCode.Exclude
